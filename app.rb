068af0240a6f451b5872a7b20e9f819269a70051
DanAparicio22 <daparicio52@gmail.com>
2017-11-11 02:54:29 -0400

    Heroku update

--- app.rb
+++ app.rb
@@ -4,37 +4,87 @@ set :bind, '0.0.0.0'
 set :port, 3000
 
 $CAVE = Cave.new
-$MENSAJE = "Te encuentras en la habitacion [" + $CAVE.getPlayer.x.to_s + "][" + $CAVE.getPlayer.y.to_s + "]"
+if($CAVE.getPlayer.x != 0) then
+  $MENSAJE = "Te encuentras en la habitacion " + $CAVE.getPlayer.x.to_s + ($CAVE.getPlayer.y+1).to_s
+else
+  $MENSAJE = "Te encuentras en la habitacion " + ($CAVE.getPlayer.y+1).to_s
+end
 $PERDER = false
+$DETECT=false
+$MENSAJESTATE=true
+
 
 get '/' do
-  $PERDER = false
-  $MENSAJE = "Te encuentras en la habitacion [" + $CAVE.getPlayer.x.to_s + "][" + $CAVE.getPlayer.y.to_s + "]"
   erb :index
 end
 
 get '/game' do
   @perdiste = $PERDER
   @cantFlechas = 3
-  @mensaje = $MENSAJE + '
-Que accion quieres realizar? Moverse, Lanzar flecha'
+  @mensaje = $MENSAJE
+  if($MENSAJESTATE==true)
+    @MensajeEstado = 'Que accion quieres realizar? 
+      * Moverse
+      * Lanzar flecha'
+  else
+    @MensajeEstado=""
+  end 
   erb :game
 end
 
 post '/action' do
   if params[:accion] == 'salir'
     $CAVE = Cave.new
-    $MENSAJE = "Te encuentras en la habitacion [" + $CAVE.getPlayer.x.to_s + "][" + $CAVE.getPlayer.y.to_s + "]"
+    if($CAVE.getPlayer.x != 0) then
+      $MENSAJE = "Te encuentras en la habitacion " + $CAVE.getPlayer.x.to_s + ($CAVE.getPlayer.y+1).to_s
+    else
+      $MENSAJE = "Te encuentras en la habitacion " + ($CAVE.getPlayer.y+1).to_s
+    end
+    if($CAVE.detectWumpus==true)
+      $Detect=true
+      @detect=$Detect
+    else
+      $Detect=false
+      @detect=$Detect
+    end
     redirect "/"
   elsif params[:accion] == 'disparar'
+    $MENSAJESTATE=false
+    $MENSAJE = "A donde desea disparar?
+    * Norte
+    * Sur
+    * Este
+    * Oeste"
     redirect "/game"
   else
     resultAction = $CAVE.movePlayer(params[:accion])
     if resultAction == 1
-      $MENSAJE = "No puedes moverte por ahi
-Te encuentras en la habitacion [" + $CAVE.getPlayer.x.to_s + "][" + $CAVE.getPlayer.y.to_s + "]"
+      $MENSAJE = "No puedes moverte por ahi\n"+
+      if($CAVE.getPlayer.x != 0) then
+        $MENSAJE = "Te encuentras en la habitacion " + $CAVE.getPlayer.x.to_s + ($CAVE.getPlayer.y+1).to_s
+      else
+        $MENSAJE = "Te encuentras en la habitacion " + ($CAVE.getPlayer.y+1).to_s
+      end
+      if($CAVE.detectWumpus == true)
+        $Detect=true
+        @detect=$Detect
+      else
+        $Detect==false
+        @detect=$Detect
+      end
     elsif resultAction == 2
-      $MENSAJE = "Te encuentras en la habitacion [" + $CAVE.getPlayer.x.to_s + "][" + $CAVE.getPlayer.y.to_s + "]"
+      if($CAVE.getPlayer.x != 0) then
+        $MENSAJE = "Te encuentras en la habitacion " + $CAVE.getPlayer.x.to_s + ($CAVE.getPlayer.y+1).to_s
+      else
+        $MENSAJE = "Te encuentras en la habitacion " + ($CAVE.getPlayer.y+1).to_s
+      end
+      if($CAVE.detectWumpus==true)
+        $Detect=true
+        @detect=$Detect
+      else
+        $Detect=false
+        @detect=$Detect
+      end
     elsif resultAction == 3
       $CAVE = Cave.new
       $PERDER = true
--- lib/cave.rb
+++ lib/cave.rb
@@ -1,7 +1,7 @@
 Player = Struct.new(:x, :y)
 
-$NUMBER_ROWS = 10
-$NUMBER_COLUMNS = 10
+$NUMBER_ROWS = 5
+$NUMBER_COLUMNS = 5
 
 class Cave
   
@@ -13,9 +13,12 @@ class Cave
     
     @player = Player.new
     @wumpus = Player.new
+    @arrow = Player.new
     posRand = Random.new
     @player.x = posRand.rand(0..($NUMBER_ROWS - 1))
     @player.y = posRand.rand(0..($NUMBER_COLUMNS - 1))
+    @arrow.x=@player.x
+    @arrow.y=@player.y
     @wumpus.x = posRand.rand(0..($NUMBER_ROWS - 1))
     @wumpus.y = posRand.rand(0..($NUMBER_COLUMNS - 1))
     
@@ -53,7 +56,7 @@ class Cave
         return 2
       end
     elsif address == 'abajo'
-      if @player.x < 9
+      if @player.x < ($NUMBER_ROWS-1)
         resetRooms
         @player.x = @player.x + 1
         if updateRooms
@@ -71,7 +74,7 @@ class Cave
         return 2
       end
     else
-      if @player.y < 9
+      if @player.y < ($NUMBER_COLUMNS-1)
         resetRooms
         @player.y = @player.y + 1
         if updateRooms
@@ -97,6 +100,53 @@ class Cave
     return false
   end
   
+  def detectWumpus
+    if( ((@player.x+1)==@wumpus.x)|| ((@player.x-1)==@wumpus.x) || ((@player.y+1)==@wumpus.y) || ((@player.y-1)==@wumpus.y) )
+      return true
+    else
+      return false
+    end
+  end
+
+  def shootArrow
+    if address == 'arriba'
+      for i in @player.y..0
+        if((@arrow.y==@wumpus.y) && (@arrow.x==@wumpus.x))
+          return true
+        else
+          @arrow.y=(@arrow.y-1)
+        end
+      end
+    elsif address == 'abajo'
+      for i in 0..@player.y
+        if((@arrow.y==@wumpus.y) && (@arrow.x==@wumpus.x))
+          return true
+        else
+          @arrow.y=(@arrow.y+1)
+        end
+      end
+
+    elsif address == 'izquierda'
+      for i in @player.x..0
+        if((@arrow.y==@wumpus.y) && (@arrow.x==@wumpus.x))
+          return true
+        else
+          @arrow.x=(@arrow.x-1)
+        end
+      end
+
+    else
+      for i in 0..@player.x
+        if((@arrow.y==@wumpus.y) && (@arrow.x==@wumpus.x))
+          return true
+        else
+          @arrow.x=(@arrow.x+1)
+        end
+      end
+    end
+    return false
+  end
+
   def getPlayer
     return @player
   end
